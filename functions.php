787db005aada2a690004fbdf2ea601999a530374
Raju Jeelaga <rajujeelaga@gmail.com>
2017-11-16 11:51:52 +0530

    functions names are changed in the theme
    functions names are changed in the theme

--- functions.php
+++ functions.php
@@ -33,44 +33,44 @@ function headerbody_classes(){
 
 
 if ( empty( get_post_thumbnail_id(get_the_ID() ) ) ){
-    add_filter("ampforwp_body_class",'my_custom_class_withoutimage');
+    add_filter("ampforwp_body_class",'tat_class_withoutimage');
 }
 
 if( $paged >1 && is_home() || is_front_page()){
-    add_filter("ampforwp_body_class",'my_custom_class_next_pages');
+    add_filter("ampforwp_body_class",'tat_class_next_pages');
 }
 if(is_search()){
-add_filter("ampforwp_body_class",'my_custom_class_search');
+add_filter("ampforwp_body_class",'tat_class_search');
 }
 if(is_single()){
      if(! has_post_thumbnail() ){
-        add_filter("ampforwp_body_class",'my_custom_class_single_img');
+        add_filter("ampforwp_body_class",'tat_class_single_img');
     }
 }
 if(ampforwp_is_front_page()){
-    add_filter("ampforwp_body_class",'my_custom_class_single_frontpage');
+    add_filter("ampforwp_body_class",'tat_class_single_frontpage');
 }
 
 }
-function my_custom_class_next_pages($previousClaases){
+function tat_class_next_pages($previousClaases){
 
     $previousClaases[] = 'home_newpage';
     return  $previousClaases;
 }
-function my_custom_class_search($previousClaases){
+function tat_class_search($previousClaases){
     $previousClaases[] = 'search_page';
     return  $previousClaases;
 }
-function my_custom_class_single_img($previousClaases){
+function tat_class_single_img($previousClaases){
     $previousClaases[] = 'without_image';
     return  $previousClaases;
 }
-function my_custom_class_withoutimage($previousClaases){
+function tat_class_withoutimage($previousClaases){
     $previousClaases[] = 'without_image';
     return  $previousClaases;
 }
 
-function my_custom_class_single_frontpage($previousClaases){
+function tat_class_single_frontpage($previousClaases){
     $previousClaases[] = 'blog_frontpage';
     return  $previousClaases;
 }
